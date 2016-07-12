e1fb651837436053b24b9198a5d62ba869090297
wangpin34 <270280522@qq.com>
2016-07-12 11:31:52 +0800

    1.2.2: Update default task for compile prod code

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
 
 gulp.task('default', function() {
-  gulp.src('lib/fs.js')
+  gulp.src('src/fs.js')
+  	.pipe(sourcemaps.init())
+  	.pipe(babel())
   	.pipe(uglify())
-  	.pipe(gulp.dest('build'));
+  	.pipe(rename('fs.min.js'))
+  	.pipe(sourcemaps.write('.'))
+  	.pipe(gulp.dest('dist'));
 });
